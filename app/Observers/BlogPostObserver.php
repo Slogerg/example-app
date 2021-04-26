f14b3336039c110e134179f8ae335908149e2dc9
Slogerg <antonslogerg@gmail.com>
2021-04-26 22:40:17 +0300

    version8

---
+++ app/Observers/BlogPostObserver.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\BlogPost;
+use Carbon\Carbon;
+
+class BlogPostObserver
+{
+    /**
+     * @param BlogPost $blogPost
+     */
+
+    public function creating(BlogPost $blogPost)
+    {
+
+        $this->setPublishedAt($blogPost);
+
+        $this->setSlug($blogPost);
+
+        $this->setHtml($blogPost);
+
+        $this->setUser($blogPost);
+
+    }
+
+
+
+    /**
+     * @param BlogPost $blogPost
+     */
+    public function updating(BlogPost $blogPost)
+    {
+
+
+
+
+        $this->setPublishedAt($blogPost);
+
+        $this->setSlug($blogPost);
+    }
+    /**
+     * Handle the BlogPost "created" event.
+     *
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return void
+     */
+    public function created(BlogPost $blogPost)
+    {
+
+    }
+
+    /**
+     * Handle the BlogPost "updated" event.
+     *
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return void
+     */
+
+
+    public function updated(BlogPost $blogPost)
+    {
+
+    }
+
+    protected function setPublishedAt(BlogPost $blogPost)
+    {
+        if(empty($blogPost->published_at) && $blogPost->is_published){
+            $blogPost->published_at = Carbon::now();
+        }
+    }
+
+    protected function setSlug(BlogPost $blogPost)
+    {
+        if (empty($blogPost->slug)){
+            $blogPost->slug = \Str::slug($blogPost->title);
+        }
+    }
+    /**
+     * Handle the BlogPost "deleted" event.
+     *
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return void
+     */
+    public function deleted(BlogPost $blogPost)
+    {
+        //
+    }
+
+    /**
+     * Handle the BlogPost "restored" event.
+     *
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return void
+     */
+    public function restored(BlogPost $blogPost)
+    {
+        //
+    }
+
+    /**
+     * Handle the BlogPost "force deleted" event.
+     *
+     * @param  \App\Models\BlogPost  $blogPost
+     * @return void
+     */
+    public function forceDeleted(BlogPost $blogPost)
+    {
+        //
+    }
+
+    public function setHtml(BlogPost $blogPost)
+    {
+        if ($blogPost->isDirty('content_raw')){
+            $blogPost->content_html = $blogPost->content_raw;
+        }
+    }
+
+    public function setUser(BlogPost $blogPost)
+    {
+        $blogPost->user_id = auth()->id() ?? BlogPost::UNKNOWN_USER;
+    }
+}
