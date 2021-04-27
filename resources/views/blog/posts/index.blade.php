377431a4cf95d8d10951e20e3faef282d4d9cdd6
Slogerg <antonslogerg@gmail.com>
2021-04-27 10:47:33 +0300

    version11

--- resources/views/blog/posts/index.blade.php
+++ resources/views/blog/posts/index.blade.php
@@ -3,22 +3,42 @@
 @section('content')
 {{--<div class="container">--}}
 
-{{--</div>--}}
-<div align="center">
-    <table>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    <h1>Всі статті блогу</h1>
+                    <table class="table table-hover">
+                <tr>
+                    <td>ID поста</td>
+                    <td>Заголовок</td>
+                    <td>Категорія</td>
+                    <td>Дата публікації</td>
+                </tr>
         @foreach($items as $item)
-
+        @if($item->is_published)
             <tr>
                 <td>{{$item->id}}</td>
                 <td><a href="{{route('blog.posts.show',$item->id)}}">{{$item->title}}</a></td>
-
+                <td>{{$item->category->title}}</td>
                 <td>{{$item->created_at}}</td>
             </tr>
+        @endif
+
+
 
 
 
         @endforeach
-    </table>
-
+        </table>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+
+
+
+
 @endsection
--- resources/views/blog/posts/show.blade.php
+++ resources/views/blog/posts/show.blade.php
@@ -2,18 +2,13 @@
 
 @section('content')
 
-
-
-
-
-
         <div class="col-sm-10">
 
-            <h1>{{$item->title}}</h1><br>
+            <h1 style="color: gray">{{$item->title}}</h1><br>
+            <h3 style="background-color: lightgray">{{$item->category->title}}</h3>
             <h4>{{$item->content_raw}}</h4>
+            <br><br><br><br><br><br><br>
+            <h5 style="color: gray">{{$item->published_at}}</h5>
         </div>
-
-
-
 
 @endsection
